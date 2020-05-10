3a45dae63c0c206c84f60d3ce8d99a6838b97950
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-10 21:15:53 +0300

    Save data in storage

--- service/weather.service.tsx
+++ service/weather.service.tsx
@@ -1,9 +1,26 @@
 import { Subject } from 'rxjs';
+import { AsyncStorage } from 'react-native';
 
 const key = '';
 const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
 
 const data = new Subject();
+
+AsyncStorage.getItem('data').then(value => {
+    if (value != null) {
+        const res = JSON.parse(value);
+        data.next(res);
+    } else {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async pos => {
+                const latitude = pos.coords.latitude;
+                const longitude = pos.coords.longitude;
+
+                getWeatherByCoords(latitude, longitude);
+            });
+        }
+    }
+})
 
 export async function getWeatherByLocation(location: string) {
     return await baseFetch(`${url}?city=${location}&key=${key}`);
@@ -21,8 +38,15 @@ function emitData(res: any) {
     data.next(res);
 }
 
+async function saveData(res: any) {
+    data.next(res);
+    const result = JSON.stringify(res);
+    AsyncStorage.setItem('data', result);
+    return res;
+}
+
 async function baseFetch(url: string) {
-    return await fetch(url).then(desterializeData).then(emitData).catch(showError);
+    return await fetch(url).then(desterializeData).then(saveData).catch(showError);
 }
 
 function desterializeData(x: Response) {
