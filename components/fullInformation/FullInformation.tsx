0e94fb12ceef84ebb9b9620c1700230683a6f3a9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-10 15:25:14 +0300

    Added title, button

--- components/fullInformation/FullInformation.tsx
+++ components/fullInformation/FullInformation.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+let { height, width } = Dimensions.get('window');
 
 export default function FullInformation() {
   return (
-      <View style={styles.container}>
-          <Text>FullInformation</Text>
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.details}>Details</Text>
+        <TouchableOpacity style={styles.show} >
+          <Icon name="chevron-up" size={20} color='black' />
+        </TouchableOpacity>
       </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
+    width,
+    height: height - 100,
+    borderColor: 'transparent',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    padding: 30
   },
+  title: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  details: {
+    color: 'black',
+    fontSize: 30
+  },
+  show: {
+    alignItems: "center",
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    borderColor: 'transparent',
+    padding: 10
+  }
 });
