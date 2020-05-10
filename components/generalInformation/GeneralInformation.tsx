dd127954274b395c7d135fd81541f6dacce80cab
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-10 14:25:44 +0300

    Added sample design

--- components/generalInformation/GeneralInformation.tsx
+++ components/generalInformation/GeneralInformation.tsx
@@ -3,13 +3,25 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function GeneralInformation() {
   return (
-      <View style={styles.container}>
-          <Text>GeneralInformation</Text>
-      </View>
+    <View style={styles.container}>
+        <Text style={styles.temperature}>4°C</Text>
+        <Text style={styles.city}>Sofia</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
   },
+  temperature: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center'
+  },
+  city: {
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center'
+  }
 });
