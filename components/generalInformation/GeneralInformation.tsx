c78b2ddd16dd35abf44746e9aaf3d014fc2d0fdd
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-10 16:06:53 +0300

    Subscribe for data

--- components/generalInformation/GeneralInformation.tsx
+++ components/generalInformation/GeneralInformation.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { getData } from '../../service/weather.service';
 
 export default function GeneralInformation() {
+  const [data, setData] = useState<any>();
+  getData().subscribe(d => setData(d));
   return (
     <View style={styles.container}>
-        <Text style={styles.temperature}>4°C</Text>
-        <Text style={styles.city}>Sofia</Text>
+        <Text style={styles.temperature}>{data ? data.data[0].temp : null}°C</Text>
+        <Text style={styles.city}>{data ? data.city_name : 'Unknown'}</Text>
     </View>
   );
 }
