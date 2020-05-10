942072c0e6f7890bb51ec0ea5929f1630fa1802a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-10 16:28:52 +0300

    Added getWeatherByCoords, getWeatherByLocation

--- service/weather.service.tsx
+++ service/weather.service.tsx
@@ -5,16 +5,24 @@ const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
 
 const data = new Subject();
 
-export async function getWeather(location: string) {
-    return await baseFetch(`${url}?city=${location}&key=${key}`).then(res => data.next(res));
+export async function getWeatherByLocation(location: string) {
+    return await baseFetch(`${url}?city=${location}&key=${key}`);
+}
+
+export async function getWeatherByCoords(lat: number, lon: number) {
+    return await baseFetch(`${url}?&lat=${lat}&lon=${lon}&key=${key}`);
 }
 
 export function getData() {
     return data;
 }
 
+function emitData(res: any) {
+    data.next(res);
+}
+
 async function baseFetch(url: string) {
-    return await fetch(url).then(desterializeData).catch(showError);
+    return await fetch(url).then(desterializeData).then(emitData).catch(showError);
 }
 
 function desterializeData(x: Response) {
